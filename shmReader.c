8f0e4e09010809b830c4a612a63f00cff9b999e8
vedantsalunke29 <vedantsalunke29@gmail.com>
2024-11-07 21:39:47 +0530

    fix

--- shmReader.c
+++ shmReader.c
@@ -1,4 +1,4 @@
-//Reader process
+// Reader process
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
@@ -6,22 +6,21 @@
 int main()
 {
 	// ftok to generate unique key
-	key_t key = ftok("shmfile",65);
+	key_t key = ftok("shmfile", 65);
 
 	// shmget returns an identifier in shmid
-	int shmid = shmget(key,1024,0666|IPC_CREAT);
+	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
 
 	// shmat to attach to shared memory
-	char *str = (char*) shmat(shmid,NULL,0);
+	char *str = (char *)shmat(shmid, NULL, 0);
 
-	printf("Data read from memory: %s\n",str);
-	
-	//detach from shared memory
+	printf("Data read from memory: %s\n", str);
+
+	// detach from shared memory
 	shmdt(str);
-	
+
 	// destroy the shared memory
-	shmctl(shmid,IPC_RMID,NULL);
-	
+	shmctl(shmid, IPC_RMID, NULL);
+
 	return 0;
 }
-
